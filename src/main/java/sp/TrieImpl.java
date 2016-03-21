5e6f68f2e3c243b9d58436429c00dd5fa185c949
Andy <andy.silv@yandex.ru>
2016-03-21 20:24:12 +0300

    fixed bug with long UTF strings

--- src/main/java/sp/TrieImpl.java
+++ src/main/java/sp/TrieImpl.java
@@ -1,11 +1,9 @@
 package sp;
 
-import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -134,7 +132,9 @@ public class TrieImpl implements Trie, StreamSerializable {
         metaObj.put("array", dump);
         metaObj.put("words_number", size);
         metaObj.put("magic", MAGIC_NUMBER);
-        dos.writeUTF(metaObj.toString());
+        byte[] ar = metaObj.toString().getBytes();
+        dos.writeInt(ar.length);
+        dos.write(ar);
         dos.close();
     }
 
@@ -142,8 +142,12 @@ public class TrieImpl implements Trie, StreamSerializable {
     @Override
     public void deserialize(InputStream in) throws IOException {
         DataInputStream dis = new DataInputStream(in);
+        int bytesNumber = dis.readInt();
+        byte[] data = new byte[bytesNumber];
+        dis.read(data);
+        String s = new String(data);
 
-        JSONObject metaObj = new JSONObject(dis.readUTF());
+        JSONObject metaObj = new JSONObject(s);
         int magicNumber = metaObj.getInt("magic");
 
         if (magicNumber != MAGIC_NUMBER) {
--- src/test/java/sp/TrieTest.java
+++ src/test/java/sp/TrieTest.java
@@ -1,15 +1,17 @@
 package sp;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-
-import sp.StreamSerializable;
-import sp.Trie;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.Test;
 
 public class TrieTest {
 
@@ -81,6 +83,39 @@ public class TrieTest {
         assertFalse(newTrie.contains("ab"));
         assertFalse(newTrie.contains(""));
         assertEquals(2, trie.size());
+    }
+    
+    @Test
+    public void testSerializationBigString() throws IOException {
+        Trie stringSet = instance();
+
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i < 50; i++) {
+            String next = generateString(50);
+            assertEquals(set.add(next), stringSet.add(next));
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ((StreamSerializable) stringSet).serialize(outputStream);
+        
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        Trie newTrie = instance();
+        ((StreamSerializable) newTrie).deserialize(inputStream);
+        
+        for (String s : set) {
+            assertTrue(newTrie.contains(s));
+        }
+        
+        assertEquals(set.size(), newTrie.size());
+    }
+    
+    public static String generateString(int length) {
+        Random rng = new Random();
+        char[] text = new char[length];
+        for (int i = 0; i < length; i++) {
+            text[i] = (char) ((char) rng.nextInt(40) + 'A');
+        }
+        return new String(text);
     }
  
  
